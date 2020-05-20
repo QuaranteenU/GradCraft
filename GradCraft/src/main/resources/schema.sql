26bd0896499d14015099c8d561fbc35bdfa845b5
Taylor Berg <me@tman0.com>
2020-05-20 16:02:43 -0400

    update columns to use utf8mb4

--- GradCraft/src/main/resources/schema.sql
+++ GradCraft/src/main/resources/schema.sql
@@ -2,25 +2,25 @@ CREATE TABLE IF NOT EXISTS ceremonies
 (
     id        INT PRIMARY KEY AUTO_INCREMENT,
     startTime DATETIME NOT NULL,
-    name      TEXT     NOT NULL
+    name      TEXT CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE = InnoDB CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS universities
 (
     id   INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) CHARACTER SET utf8mb4 NOT NULL
 ) ENGINE = InnoDB CHARACTER SET utf8mb4;
 
 CREATE TABLE IF NOT EXISTS graduates
 (
     id            INT PRIMARY KEY AUTO_INCREMENT,
-    name          TEXT NOT NULL,
-    email         TEXT NOT NULL,
-    pronunciation TEXT,
-    degreeLevel   TEXT NOT NULL,
-    honors        TEXT,
-    major         TEXT NOT NULL,
-    seniorQuote   TEXT,
+    name          TEXT CHARACTER SET utf8mb4 NOT NULL,
+    email         TEXT CHARACTER SET utf8mb4 NOT NULL,
+    pronunciation TEXT CHARACTER SET utf8mb4,
+    degreeLevel   TEXT CHARACTER SET utf8mb4 NOT NULL,
+    honors        TEXT CHARACTER SET utf8mb4,
+    major         TEXT CHARACTER SET utf8mb4 NOT NULL,
+    seniorQuote   TEXT CHARACTER SET utf8mb4,
     university    INT  NOT NULL,
     ceremony      INT  NOT NULL,
     uuid          VARCHAR(38),
