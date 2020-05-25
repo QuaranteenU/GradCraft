91d2f596481bf8a4ba3f9a559dc46fa0d74cc570
Taylor Berg <me@tman0.com>
2020-05-25 15:41:35 -0400

    add gpl v3 license and headers

--- GradCraft/src/main/java/university/quaranteen/gradcraft/ceremony/commands/StartCommand.java
+++ GradCraft/src/main/java/university/quaranteen/gradcraft/ceremony/commands/StartCommand.java
@@ -1,3 +1,21 @@
+/*
+    This file is part of GradCraft, by the Quaranteen University team.
+    https://quaranteen.university
+
+    GradCraft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GradCraft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GradCraft.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package university.quaranteen.gradcraft.ceremony.commands;
 
 import com.bergerkiller.bukkit.common.MessageBuilder;
